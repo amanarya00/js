7f05820308c8acab4f919487d3791bb7ccae25ad
amanarya00 <amanarya1386@gmail.com>
2026-01-05 19:06:23 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 123345
+let acountEmail ="aman@google.com"
+var accountPassword ="12345678"
+accountcity = "Jaipur"
+
+//accountId = 2 //not allowed 
+
+accountEmail = "hajska@google.com"
+accountPassword = "09876543"
+accountcity = "kolkata"
+
+console.log(accountId);
+console.table([  accountId ,accountEmail,accountPassword,accountcity ])
+
+
+ 
+
